bcba55987f4e6039c63330e2edcd6dbc0e8347d9
DenisVahrameev <66534590+DenisVahrameev@users.noreply.github.com>
2023-05-11 21:26:42 +0300

    4

--- js/task-04.js
+++ js/task-04.js
@@ -1,5 +1,39 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+const input = document.querySelector('#controls input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxes = document.querySelector('#boxes');
+
+function createBoxes(amount) {
+  // Розміри найпершого <div> - 30px на 30px
+  let boxSize = 30;
+
+  for (let i = 0; i < amount; i += 1) {
+    // Створення нового <div>
+    const box = document.createElement('div');
+
+    // Випадковий колір фону
+    box.style.backgroundColor = getRandomHexColor();
+
+    // Розмір <div>
+    box.style.width = `${boxSize}px`;
+    box.style.height = `${boxSize}px`;
+
+    // Додавання <div> в div#boxes
+    boxes.appendChild(box);
+
+    // Збільшення розміру на 10px для наступного <div>
+    boxSize += 10;
+  }
 }
+
+function destroyBoxes() {
+  // Очистка вмісту div#boxes
+  boxes.innerHTML = '';
+}
+
+// Обробники подій для кнопок
+createBtn.addEventListener('click', () => {
+  createBoxes(input.value);
+});
+
+destroyBtn.addEventListener('click', destroyBoxes);
