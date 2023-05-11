ae5c9d3d169796c736f21e46e132718e12eff9ac
DenisVahrameev <66534590+DenisVahrameev@users.noreply.github.com>
2023-05-11 20:29:25 +0300

    2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ul = document.querySelector("#ingredients");
+
+const createListItem = (text) => {
+  const li = document.createElement("li");
+  li.textContent = text;
+  li.classList.add("item");
+  return li;
+};
+
+const liElements = ingredients.map((ingredient) => createListItem(ingredient));
+
+ul.append(...liElements);
